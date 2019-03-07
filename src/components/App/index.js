4aafdfcc12a212e757efbdbedcb32f38166960dc
Christopher McCormack <mccormack.christopher@gmail.com>
2019-03-06 22:37:21 -0700

    feat: added filechooser

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,27 @@
 import React from "react";
-
 import "./App.scss";
-import "../../assets/images/react-logo.png";
 
 const App = props => {
+  const handleFile = e => {
+    const file = e.target.files.item(0);
+
+    csv()
+      .fromFile(file)
+      .then(json => {
+        console.log(json);
+      });
+    // const reader = new FileReader(file);
+    // reader.onload = () => {
+    //   console.log(reader.result);
+    // };
+    // reader.readAsText(file);
+  };
+
   return (
     <div>
-      <img src="assets/images/react-logo.png" />
-      <h1 id="title">Hello, React!</h1>;
+      <h1 id="title">CSV Migration Tool</h1>;
+      <input type="file" accept="text/csv" onChange={handleFile} />
+      <input type="file" accept="text/csv" onChange={handleFile} />
     </div>
   );
 };
