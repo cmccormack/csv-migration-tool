f76845cde9b2a2cb191ff1743a1f34c183c232bb
Christopher McCormack <mccormack.christopher@gmail.com>
2019-03-06 20:05:18 -0700

    feat: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: ["prettier", "prettier/react"],
+  parserOptions: {
+    ecmaVersion: 2018,
+    ecmaFeatures: {
+      jsx: true
+    },
+    sourceType: "module"
+  },
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        trailingComma: "es5",
+        tabWidth: 2
+      }
+    ]
+  }
+};
---
+++ README.md
@@ -0,0 +1,7 @@
+# CSV Migration Tool
+
+## Description
+
+This tool is to migrate data from a CSV to another CSV with similar-but-different columns.
+
+The tool will attempt to merge all columns with the same name between the two documents while maintaining the layout of the new CSV file to which data is migrating to prevent issues importing the document into other tools.
